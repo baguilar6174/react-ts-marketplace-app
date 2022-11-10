3a9a4fd925e92aa58c76939d0337adb9a82af486
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2022-11-09 22:01:42 -0500

    feat: Initial commit, add prettier and eslint and create file directory

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+		'standard-with-typescript',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {},
+};
